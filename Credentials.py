21f6e558dc1c87556a2dc9b9e9b9225c9f1a6c79
WanzhuShi <40009167+WanzhuShi@users.noreply.github.com>
2018-06-06 18:35:14 -0500

    Update Credentials.py

--- Credentials.py
+++ Credentials.py
@@ -1,6 +1,6 @@
 #Enter Your Credentials Here
-#poshmark_email = "wanzhu.s@gmail.com"
-#poshmark_password = "Swz0217Gyj1128"
+#poshmark_email = ""
+#poshmark_password = ""
 
 #Rename this file "credentials.py"
 #mv example_credentials.py credentials.py
